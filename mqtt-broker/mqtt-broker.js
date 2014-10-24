a89fc85fd3f8fde78886dbce4b6b6196290ea9dc
Lars Schütze <dev@larsschuetze.de>
2014-10-24 13:01:24 +0200

    first working example of fake-rpi + server + webclient

---
+++ mqtt-broker/mqtt-broker.js
@@ -0,0 +1,66 @@
+var mosca = require('mosca');
+
+var moscaSettings = {
+	port: 1883           //mosca (mqtt) port
+};
+
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({port: 8080});
+
+wss.broadcast = function(data) {
+    console.log(data);
+
+	for(var i in this.clients)
+    {
+		this.clients[i].send(data);
+    }
+};
+
+var server = new mosca.Server(moscaSettings);   //here we start mosca
+server.on('ready', setup);  //on init it fires up setup()
+
+// fired when the mqtt server is ready
+function setup() {
+	console.log('Mosca server is up and running')
+}
+
+// fired when a message is published
+server.on('published', function(packet, client) {
+    if(client !== undefined) {
+    	var json = '{"action":"published' +
+        '","client":'+ JSON.stringify(client.id) +
+        ',"topic":'+ JSON.stringify(packet.topic) +
+        ',"payload":'+ JSON.stringify(packet.payload) +
+        '}';
+
+        wss.broadcast(json);
+    }
+});
+
+// fired when a client connects
+server.on('clientConnected', function(client) {
+	console.log('Client Connected:', client.id);
+
+	var json = '{"action":"connected","client":' + JSON.stringify(client.id) +'}';
+	//var obj = JSON.parse(json);
+
+	wss.broadcast(json);
+});
+
+// fired when a client disconnects
+server.on('clientDisconnected', function(client) {
+	console.log('Client Disconnected:', client.id);
+
+	var json = '{"action":"disconnected","client":' + JSON.stringify(client.id) +'}';
+
+	wss.broadcast(json);
+});
+
+/*
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+    });
+    ws.send('something');
+});
+*/
---
+++ mqtt-webclient/js/client.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    var wsUri = "ws://localhost:8080/";
+    var clients = [];
+
+    $("#connect").click(function() {
+        startWebSocket();
+    });
+
+    $("#disconnect").click(function() {
+        closeWebSocket();
+    });
+
+    function startWebSocket() {
+        websocket = new WebSocket(wsUri);
+
+        websocket.onopen = function(evt) {
+            onOpen(evt)
+        };
+
+        websocket.onclose = function(evt) {
+            onClose(evt)
+        };
+
+        websocket.onmessage = function(evt) {
+            var data = evt.data;
+            onMessage(data);
+        };
+
+        websocket.onerror = function(evt) {
+            onError(evt)
+        };
+    }
+
+    function onOpen(evt) {
+        writeToScreen("CONNECTED");
+        doSend("WebSocket rocks");
+    }
+
+    function onClose(evt) {
+        writeToScreen("DISCONNECTED");
+    }
+
+    function onMessage(data) {
+        if(data !== "") {
+            var msg = JSON.parse(data);
+
+            switch(msg.action) {
+                case "published":
+                    break;
+                case "connected":
+                    //writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.client);
+                    addClient(msg);
+                    break;
+                case "disconnected":
+
+                    break;
+            }
+        }
+        //writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.client +'</span>');
+    }
+
+    function onError(evt) {
+        writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+    }
+
+    function doSend(message) {
+        writeToScreen("SENT: " + message);
+        websocket.send(message);
+    }
+
+    function addClient(json) {
+        $("#clients").append('<li>'+json.client+'</li>')
+    }
+
+    function writeToScreen(message) {
+        var pre = document.createElement("p");
+        pre.style.wordWrap = "break-word";
+        pre.innerHTML = message;
+        output.appendChild(pre);
+    }
+
+    function closeWebSocket() {
+        websocket.close();
+    }
+});
