d48b5576f6f3b590323c1412b9ca0676a221e821
Lars Schütze <dev@larsschuetze.de>
2014-11-19 10:13:18 +0100

    first structural changes towards being node js plugin

--- mqtt-broker/mqtt-broker.js
+++ mqtt-broker/mqtt-broker.js
@@ -1,66 +1,67 @@
-var mosca = require('mosca');
+var mqtt = function()
+{
+    var mosca = require('mosca');
+    var server = new mosca.Server(moscaSettings);
 
-var moscaSettings = {
-	port: 1883           //mosca (mqtt) port
-};
+    var moscaSettings = {
+	   port: 1883
+    };
 
-var WebSocketServer = require('ws').Server,
-wss = new WebSocketServer({port: 8080});
+    server.on('ready', setup);
 
-wss.broadcast = function(data) {
-    console.log(data);
 
-	for(var i in this.clients)
-    {
-		this.clients[i].send(data);
+    function setup() {
+	   console.log('MQTT: Mosca server is up and running')
     }
-};
 
-var server = new mosca.Server(moscaSettings);   //here we start mosca
-server.on('ready', setup);  //on init it fires up setup()
+    // fired when a message is published
+    server.on('published', function(packet, client) {
+        if(client !== undefined) {
+    	   var json = '{"action":"published' +
+            '","client":'+ JSON.stringify(client.id) +
+            ',"topic":'+ JSON.stringify(packet.topic) +
+            ',"payload":'+ JSON.stringify(packet.payload) +
+            '}';
 
-// fired when the mqtt server is ready
-function setup() {
-	console.log('Mosca server is up and running')
-}
-
-// fired when a message is published
-server.on('published', function(packet, client) {
-    if(client !== undefined) {
-    	var json = '{"action":"published' +
-        '","client":'+ JSON.stringify(client.id) +
-        ',"topic":'+ JSON.stringify(packet.topic) +
-        ',"payload":'+ JSON.stringify(packet.payload) +
-        '}';
-
-        wss.broadcast(json);
-    }
-});
-
-// fired when a client connects
-server.on('clientConnected', function(client) {
-	console.log('Client Connected:', client.id);
-
-	var json = '{"action":"connected","client":' + JSON.stringify(client.id) +'}';
-	//var obj = JSON.parse(json);
-
-	wss.broadcast(json);
-});
-
-// fired when a client disconnects
-server.on('clientDisconnected', function(client) {
-	console.log('Client Disconnected:', client.id);
-
-	var json = '{"action":"disconnected","client":' + JSON.stringify(client.id) +'}';
-
-	wss.broadcast(json);
-});
-
-/*
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
+            wss.broadcast(json);
+        }
     });
-    ws.send('something');
-});
-*/
+
+    // fired when a client connects
+    server.on('clientConnected', function(client) {
+    	console.log('Client Connected:', client.id);
+
+    	var json = '{"action":"connected","client":' + JSON.stringify(client.id) +'}';
+    	//var obj = JSON.parse(json);
+
+    	wss.broadcast(json);
+    });
+
+    // fired when a client disconnects
+    server.on('clientDisconnected', function(client) {
+    	console.log('Client Disconnected:', client.id);
+
+    	var json = '{"action":"disconnected","client":' + JSON.stringify(client.id) +'}';
+
+    	wss.broadcast(json);
+    });
+
+    /*
+    wss.on('connection', function(ws) {
+        ws.on('message', function(message) {
+            console.log('received: %s', message);
+        });
+        ws.send('something');
+    });
+    */
+
+    var _receive = function(controls, callback) {
+
+    };
+
+    return {
+        receive : _receive;
+    };
+}();
+
+module.exports = mqtt;
